f8d3223bd297ca5def4590a8a840c097c4d5f59a
koby0max <matloobahmedaz91@gmail.com>
2025-07-14 20:49:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,31 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Avalon Backend is running!');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
 // Node.js + Socket.IO backend server
